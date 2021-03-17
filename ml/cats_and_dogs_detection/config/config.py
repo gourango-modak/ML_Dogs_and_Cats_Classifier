f713925813ffd4d1a4efbee82d7dcac12d527f18
gourango_modak <gourango.modak.2@gmail.com>
2021-03-17 13:21:07 +0600

    update ml

--- ml/cats_and_dogs_detection/config/config.py
+++ ml/cats_and_dogs_detection/config/config.py
@@ -1,4 +1,5 @@
-DATA_DIR = "/datasets/train/"
-OUTPUT = "/output/features.hdf5"
+DATA_DIR = "/content/ML_Dogs_and_Cats_Classifier/ml/cats_and_dogs_detection/datasets/train"
+OUTPUT = "/content/ML_Dogs_and_Cats_Classifier/ml/cats_and_dogs_detection/output/features.hdf5"
 
 BUFFER_SIZE = 1000
+BATCH_SIZE = 128
