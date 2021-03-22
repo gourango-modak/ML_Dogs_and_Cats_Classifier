2967c761118858cddcd56dd56bb556ec4a906d31
gourango_modak <gourango.modak.2@gmail.com>
2021-03-22 14:54:31 +0600

    update validation

--- web_app/core/static/core/js/style.js
+++ web_app/core/static/core/js/style.js
@@ -12,23 +12,54 @@ $(document).ready(function(){
         }
     }
     $("#image_data").change(function() {
-        readURL(this);
+      $(".alert").hide();
+      
+      var val = $(this).val();
+      switch(val.substring(val.lastIndexOf('.') + 1).toLowerCase()){
+          case 'gif': case 'jpg': case 'png':
+              readURL(this);
+              $(".alert").hide();
+              break;
+          default:
+              $(this).val('');
+              $(".alert").html("It is not an image file");
+              $(".alert").show();
+              $("#image_src").attr("src", "");
+              break;
+      }
     });
+    
+    $(".alert").hide();
+    $("#predict").on('click', function() {
+      if ($("#image_data").val() == "") {
+        $(".alert").html("Please Input an Image");
+        $(".alert").show();
+        $('#predict').blur();
+      } else {
+        $(".main_content").css("filter", "blur(3px)");
+        $(".spinner_loader").css("display", "block");
+      }
+    });
+    
     $("#upload_image").submit(function(e) {
       e.preventDefault();
-      // $("#spinner").html('<i class="fas fa-spinner fa-xs"></i>');
-      var data = new FormData(this);
-      $.ajax({
-        url: "image/",
-        type: 'POST',
-        data: data,
-        contentType: false,
-        processData: false,
-        success: function(result){
-          console.log(result);
-          $(".class_label").html(result);
-        }
-      });
+      if ($("#image_data").val() != "") {
+        var data = new FormData(this);
+        $.ajax({
+          url: "image/",
+          type: 'POST',
+          data: data,
+          contentType: false,
+          processData: false,
+          success: function(result){
+            $(".class_label").html(result);
+            $(".main_content").css("filter", "blur(0px)");
+            $(".spinner_loader").css("display", "none");
+            $("#image_data").val('');
+            $('#predict').blur();
+          }
+        });
+      }
     
   
     });
